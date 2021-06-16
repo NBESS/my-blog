4e8d567db597afd66e9cf8d12fa151d79f7c2562
NBESS <nicholasdbess@gmail.com>
2021-06-16 13:08:40 -0400

    Pass posts and setPosts to CreatePost from App as props

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { PostList } from './post/PostList';
 import { CreatePost } from './post/CreatePost';
 import { UserBar } from './user/UserBar';
 
-const posts = [
+const defaultPosts = [
   {
     title: 'Post 1 Title',
     content: 'This is going better than expected!',
@@ -21,11 +21,12 @@ const posts = [
 
 function App() {
   const [user, setUser] = useState('');
+  const [posts, setPosts] = useState(defaultPosts);
   return (
     <div style={{ padding: 8 }}>
       <UserBar user={user} setUser={setUser} />
       <br />
-      {user && <CreatePost user={user} />}
+      {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
       <br />
       <hr />
       <PostList posts={posts} />
