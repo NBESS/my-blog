44968acb8a1d14b53fa74e1617c0d2addbec122d
NBESS <nicholasdbess@gmail.com>
2021-06-20 05:23:37 -0400

    Refactor App to contain new HompePage component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useReducer, useEffect, useState } from 'react';
 
-import { PostList } from './post/PostList';
-import { useResource } from 'react-request-hook';
 import HeaderBar from './pages/HeaderBar';
+import HomePage from './pages/HomePage';
 import { ThemeContext, StateContext } from './contexts';
 import appReducer from './reducers';
 import './App.css';
@@ -18,23 +17,7 @@ function App() {
     error: '',
   });
 
-  const { user, error } = state;
-
-  const [posts, getPosts] = useResource(() => ({
-    url: '/posts',
-    method: 'get',
-  }));
-
-  useEffect(getPosts, []);
-
-  useEffect(() => {
-    if (posts && posts.error) {
-      dispatch({ type: 'POSTS_ERROR' });
-    }
-    if (posts && posts.data) {
-      dispatch({ type: 'FETCH_POSTS', posts: posts.data.reverse() });
-    }
-  }, [posts]);
+  const { user } = state;
 
   useEffect(() => {
     if (user) {
@@ -51,8 +34,7 @@ function App() {
           <HeaderBar setTheme={setTheme} />
           <br />
           <hr />
-          {error && <b>{error}</b>}
-          <PostList />
+          <HomePage />
         </div>
       </ThemeContext.Provider>
     </StateContext.Provider>
